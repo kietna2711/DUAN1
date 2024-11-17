ae61bc4a5abfb3e87e91471d1bbdc332baf29989
kietna2711 <kietanh27112005@gmail.com>
2024-11-17 13:12:52 +0700

    Admin

---
+++ models/m_database.php
@@ -0,0 +1,49 @@
+<?php
+class database{
+
+   public $servername = "localhost";
+   public $username = "root";
+   public $password = "";
+   public $dbname = "lab4";
+   public $conn;
+   public function __construct(){
+    try {
+        $conn = new PDO("mysql:host=".$this->servername.";dbname=".$this->dbname, $this->username, $this->password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
+      $this->conn = $conn;
+   }
+   public function query($sql,$params=[]){
+    $statement = $this->conn->prepare($sql);
+    $statement->execute($params);
+    return $statement;
+   }
+   // láy tất cả
+   public function getAll($sql){
+    $statement=$this->query($sql);
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+   }
+   // lấy 1
+   public function getOne($sql){
+    $statement=$this->query($sql);
+    return $statement->fetch(PDO::FETCH_ASSOC);
+   }
+   // tạo thêm
+   public function insert($sql){
+    $this->query($sql);
+    return $this->conn->lastInsertId();
+   }
+   // sữa lại
+   public function update($sql){
+    $this->query($sql);
+   }
+   // xóa
+   public function delete($sql){
+    $this->query($sql);
+   }
+}
+
+?>
